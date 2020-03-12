b0c09ef467c1d1418968078231ed8a0868118659
Munaa <munaa.tsetsegmaa@gmail.com>
2020-03-12 23:24:46 +0800

    STATE bolon Search button hiilee

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,34 @@
 require("@babel/polyfill");
 import Search from "./model/search";
 
-let search = new Search("pasta");
-search.doSearch().then(r => console.log("Ur dun ; " + r));
+/*
+ * Web app
+ * Хайлтын query , ur dun
+ * Tuhain uzuulj baigaa jor
+ * Like lsan jor
+ * Zahialj baigaa joriin nairlaga
+ *
+ *
+ *
+ */
+const state = {};
+const controlSearch = async () => {
+  //// 1. Web-ees hailtiin tulhuur ugiig gargaj abna
+  const query = "pizza";
+
+  if (query) {
+    //// 2. Shine hailtiin obektiig uusgej ogno
+    state.search = new Search(query);
+
+    //// 3. Hailt hiihed zoriulj delgetsig UI beltgene
+
+    //// 4. Hailtiig guitsetgene
+    await state.search.doSearch();
+    //// 5. Hailtiin ur dung delgetsend uzuulne
+    console.log(state.search.result);
+  }
+};
+document.querySelector(".search").addEventListener("submit", e => {
+  e.preventDefault();
+  controlSearch();
+});
