b061b47b2db922b637c3f2bccc3193919fe160a5
Munaa <munaa.tsetsegmaa@gmail.com>
2020-03-13 13:08:29 +0800

    Joriig delgetsend gargalaa

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Search from "./model/search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
 import Recipe from "./model/Recipe";
+import { renderRecipe, clearRecipe } from "./view/recipeVeiw";
 
 /*
  * Web app
@@ -49,5 +50,27 @@ elements.pageButtons.addEventListener("click", e => {
   }
 });
 
-const r = new Recipe(47746);
-r.getRecipe();
+/*
+Joriin controller
+*/
+//// Hash oorchlogdoj baigaag barij abna
+const controlRecipe = async () => {
+  //// 1. URL-aas ID iig salgana
+  const id = window.location.hash.replace("#", "");
+  //// 2 Joriin modeliig uusgene
+  state.recipe = new Recipe(id);
+  //// 3 Delgetsend beltgene
+
+  clearRecipe();
+  renderLoader(elements.recipeDiv);
+  //// 4 Joroo tataj abchirna
+  await state.recipe.getRecipe();
+  //// Joriig guitsetgeh hugatsaa bolon ortsiig tootsoolno
+  clearLoader();
+  state.recipe.calcTime();
+  state.recipe.calcHuniiToo();
+  //// Joroo delgetsend gargana
+  renderRecipe(state.recipe);
+};
+window.addEventListener("hashchange", controlRecipe);
+window.addEventListener("load", controlRecipe);
--- src/js/model/Recipe.js
+++ src/js/model/Recipe.js
@@ -16,4 +16,11 @@ export default class Recipe {
     this.title = result.data.recipe.title;
     this.social_rank = result.data.recipe.social_rank;
   }
+  calcTime() {
+    //// nairlaga burt oiroltsoogoor 5 minut zartsuulna
+    this.time = this.ingredients.length * 5;
+  }
+  calcHuniiToo() {
+    this.huniiToo = 4;
+  }
 }
